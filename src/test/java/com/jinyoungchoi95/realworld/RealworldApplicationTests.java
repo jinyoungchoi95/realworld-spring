60d9be4f8f280a0aa77afcc4d8f3feb93af5a62e
최진영 <jinyoungchoi95@gmail.com>
2022-05-22 14:58:41 +0900

    project initial setup (#2)
    * [#1] feat: project initial setup
    
    * [#1] docs: add github pull request template

---
+++ src/test/java/com/jinyoungchoi95/realworld/RealworldApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jinyoungchoi95.realworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealworldApplicationTests {
+
+    @Test
+    void main() {
+        RealworldApplication.main(new String[] {});
+    }
+
+}
